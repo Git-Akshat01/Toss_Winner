4041a0053fafd91995a675b86dbb4d509b2632da
Akshat Tiwari <akshattiwari0901@gmail.com>
2023-08-24 11:03:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Won";
+}
+else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Won";
+}
+else {
+    document.querySelector("h1").innerHTML = "🏁 It's a Draw";
+}
